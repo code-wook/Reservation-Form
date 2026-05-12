8c1bca80681a28cd87c1a06f58dc1ccfaefbc6f2
code-wook <jamesgerali@gmail.com>
2026-05-12 16:54:26 +0800

    Updated UI

--- resources/views/components/form-navigation.blade.php
+++ resources/views/components/form-navigation.blade.php
@@ -1,24 +1,19 @@
-<div class="flex flex-col sm:flex-row justify-center sm:justify-between w-full sm:w-2/3 mx-auto mt-8 gap-4 sm:gap-8">
+<div class="flex flex-col-reverse sm:flex-row justify-center sm:justify-between w-full sm:w-2/3 mx-auto mt-8 gap-4 sm:gap-8">
+    
+    <!-- BACK BUTTON -->
     <button type="button"
-       class="w-full sm:w-48 text-center py-3 rounded-full border-2 border-red-600 
-              text-red-800 font-semibold bg-white 
-              mb-4 btn-back">
+        class="w-full sm:w-48 text-center py-3 rounded-full border-2 border-red-600 
+        text-red-800 font-semibold bg-white mb-4 btn-back transition-all">
         {{ $backText ?? 'Return' }}
     </button>
 
-    @if($nextText === 'Submit')
-        <button type="submit" 
-            class=" w-full sm:w-48 py-3 rounded-full border-2 border-red-600 
-                   text-white font-semibold bg-red-800 
-                   mb-4">
-            {{ $nextText ?? 'Submit' }}
-        </button>
-    @else
-        <button type="button" 
-            class="w-full sm:w-48 py-3 rounded-full border-2 border-red-600 
-                   text-white font-semibold bg-red-800 
-                   mb-4 next-btn">
-            {{ $nextText ?? 'Next' }}
-        </button>
-    @endif
+    <!-- NEXT / SUBMIT BUTTON (controlled by JS) -->
+    <button type="button" 
+        class="w-full sm:w-48 py-3 rounded-full border-2 border-red-600 
+               text-white font-semibold bg-red-800 mb-4 next-btn 
+               transition-all duration-200"
+        id="nextBtn">
+        Next
+    </button>
+
 </div>
